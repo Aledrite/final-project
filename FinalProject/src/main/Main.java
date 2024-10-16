d3f3a65c246d798bf537e9964e5692018dcbcde0
jaret <jaret@DESKTOP-NKODNGE>
2024-10-17 02:41:04 +0800

    Commit 2

--- FinalProject/src/main/Main.java
+++ FinalProject/src/main/Main.java
@@ -1,10 +1,35 @@
 package main;
 
+import java.util.Scanner;
+import visuals.Colors;
 
-
-public class Main {
+public class Main 
+{
+	
 	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(Colors.CYAN + "Welcome to Atom." + Colors.RESET);
+		System.out.println(Colors.GREEN + "Would you like to start the program? Y/N" + Colors.RESET);
+		
+		Boolean programRunning = false;
+		
+		char answer = sc.nextLine().charAt(0);
+		
+		answer = Character.toUpperCase(answer);
+		
+		if (answer == "Y".charAt(0)) {programRunning = true;}
+		
+		while (programRunning) {}
+		
+		System.out.println("Program ended.");
+		
+	}
+	
+	private static void Calculator() 
 	{
 		
 	}
+	
 }
